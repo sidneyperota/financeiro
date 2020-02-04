bb545991650b0dbf6f198ef9929a44d9aec024ba
sidneyperota <sidneyperota@gmail.com>
2020-02-04 17:56:18 -0300

    criacao do jframe principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,12 @@
+import visao.FrmPrincipal;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		FrmPrincipal frmPrincipal = new FrmPrincipal(); 
+
+		frmPrincipal.setVisible(true);
 
 	}
 
---
+++ src/visao/FrmPrincipal.java
@@ -0,0 +1,79 @@
+package visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JDesktopPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+
+public class FrmPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmPrincipal frame = new FrmPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmPrincipal() {
+		setTitle("Controle de Caixa");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(450, 200, 754, 477);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnCaixa = new JMenu("Caixa");
+		menuBar.add(mnCaixa);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Consulta");
+		mnCaixa.add(mntmNewMenuItem);
+		
+		JMenuItem mntmLanamento = new JMenuItem("Lan\u00E7amento");
+		mnCaixa.add(mntmLanamento);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Altera\u00E7\u00E3o");
+		mnCaixa.add(mntmNewMenuItem_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setVgap(20);
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		panel_1.add(desktopPane, BorderLayout.CENTER);
+		
+		JPanel panel_2 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_2.getLayout();
+		flowLayout_1.setVgap(20);
+		contentPane.add(panel_2, BorderLayout.SOUTH);
+	}
+}
